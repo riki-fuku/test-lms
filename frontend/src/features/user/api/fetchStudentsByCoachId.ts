c5fbd52aaa5de72aa31bb1cb35776273adfca2c4
riki-fuku <chsgrn.9529@gmail.com>
2025-09-17 21:22:48 +0900

    FEのビルドエラー解消

--- frontend/src/features/user/api/fetchStudentsByCoachId.ts
+++
@@ -1,9 +0,0 @@
-import type { CoachStudent } from '@/features/user/types/CoachStudent'
-import { Http } from '@/lib/api-client'
-import type { AxiosResponse } from 'axios'
-
-export default function fetchStudentsByCoachId(coachId: string) {
-  return Http.axios()
-    .get<AxiosResponse<CoachStudent[]>>(`/api/v1/coaches/${coachId}/students`)
-    .then((res) => res.data.data)
-}
--- frontend/src/features/user/api/forceDeleteUser.ts
+++
@@ -1,7 +0,0 @@
-import { Http } from '@/lib/api-client'
-
-export default async function forceDeleteUser(email: string) {
-  return Http.axios().delete(`/api/users/force-delete`, {
-    params: { email },
-  })
-}
